aacd8ba1d5eebf6ea3ee6c182df8cc1ae9c1200e
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2016-02-12 17:51:40 -0500

    adding decoding functions

---
+++ src/decode.js
@@ -0,0 +1,69 @@
+import { getRangeFor } from './encode';
+
+var common = `
+vec2 getCoord(vec2 res, float col, float row) {
+    return fract(vec2(col,row+.5)/res);
+}`+'\n';
+
+var uint = common + `
+float getUInt(sampler2D tex, vec2 res, float col, float row) {
+    highp vec4 value = texture2D(tex, coord);
+    return (value.x*255.)+(value.y*65025.)+(value.z*16581375.);
+}`;
+
+var ufloat = common + `
+float getUFloat(sampler2D tex, vec2 res, float col, float row) {
+    highp vec4 value = texture2D(tex, coord);
+    return ((value.x*255.)+(value.y*65025.)+(value.z*16581375.))/16581375.;
+}`;
+
+var position = common + `
+vec2 getPosition(sampler2D tex, vec2 res, float col, float row) {
+    vec2 coord = getCoord(res, col, row);
+    highp vec4 value = texture2D(tex, coord);
+    float x = (value.r*255+value.g*65025.)/65025.;
+    float y = (value.b*255.+value.a*65025.)/65025.;
+    return vec2(x,y);
+}`+'\n';
+
+var color = common + `
+vec4 getColor(sampler2D tex, vec2 res, float col, float row) {
+    vec2 coord = getCoord(res, col, row);
+    return texture2D(tex, coord);
+}`+'\n';
+
+var decode = {
+    uint: uint,
+    int: uint + `
+float getInt(sampler2D tex, vec2 res, float col, float row) {
+    return getUInt(tex,res,col,row)-8290688.;
+}`,
+    ufloat: ufloat,
+    float: ufloat + `
+float getUFloat(sampler2D tex, vec2 res, float col, float row) {
+    return (getUFloat(tex,res,col,row)-.5)*2.;
+}`,
+    number: common + `
+vec3 getElements(sampler2D tex, vec2 coord) {
+    highp vec4 value = texture2D(tex, coord);
+    highp float uint = (value.x*255.)+(value.y*65025.)+(value.z*16581375.);
+    float press = ceil(value.a*255.)-244.;
+    return vec3(uint, abs(press), sign(press));
+}
+float getNumber(sampler2D tex, vec2 res, float col, float row) {
+    vec2 coord = getCoord(res, col, row);
+    highp vec3 elements = getElements(tex, coord);
+    return elements.x * pow(10.,-floor(elements.y)) * elements.z;
+}`+'\n',
+    position: position,
+    color: color,
+    vec2: position,
+    vec3: color,
+    vec4: color
+}
+
+export function getDecodeFunctionFor (type) {
+    return decode[type];
+}
+
+window.getDecodeFunctionFor = getDecodeFunctionFor;
---
+++ src/encode.js
@@ -0,0 +1,115 @@
+import { clamp, map, decimals } from './common';
+
+// Default Ranges
+var ranges = {
+	number: [-16581375,16581375],
+	char: 	[-128, 127],
+	uchar: 	[0, 255],
+	int: 	[-8290688, 8290687],
+	uint: 	[0, 16581375],
+	float: 	[-1, 1],
+	ufloat: [0, 1],
+	color: 	[0, 255],
+	rgb: 	[0, 255],
+	position: [-1, 1],
+	vec2: 	[-1, 1],
+	vec3: 	[-1, 1],
+	vec4: 	[-1, 1]
+};
+
+export function getRangeFor (type) {
+	return ranges[type];
+}
+
+function clampValue (value, range) {
+	if (value < range[0] || value > range[1]) {
+		console.warn('Value',value,'is out of range',range,'will be clamped to', clamp(value,range[0],range[1]));
+		return clamp(value,range[0],range[1]);
+	}
+	else {
+		return value;
+	}
+}
+
+function normalizeValue (value, range) {
+	return map(value,range[0],range[1],0,1);
+}
+
+export function encodeValue (value, type, range) {
+	if (!range) {
+		range = getRangeFor(type);
+	}
+
+	if (typeof value === 'number') {
+		value = clampValue(value, range);
+		if (type !== 'number') {
+			// "Number" type don't need normaliztion because tries to set the presition dinamically
+			value = normalizeValue(value, range);
+		}
+	} 
+	else if (Array.isArray(value)) {
+		for (let i in value) {
+			value[i] = clampValue(value[i], range);
+			if (type !== 'number') {
+				// "Number" type don't need normaliztion because tries to set the presition dinamically
+				value[i] = normalizeValue(value[i], range);
+			}
+		}
+	} 
+	else {
+		console.log('Value type could not be read', value, type, range);
+		return;
+	}
+	
+	if (type === 'uchar' || type === 'char') {
+		value *= 255;
+	    return [
+	        Math.floor(value),
+	        Math.floor(value),
+	        Math.floor(value),
+	        255
+	    ];
+	}
+	else if (type === 'int' || type === 'uint'  || type === 'float' || type === 'ufloat') {
+		value = value*16581375;
+	    return [
+	        Math.floor(value%255),
+	        Math.floor(value/255)%255,
+	        Math.floor(value/(255*255)),
+	        255
+	    ];
+	}
+	else if (type === 'number') {
+		let s = Math.sign(value);
+		let d = decimals(value)+1.;
+		let uint = Math.abs(value) * Math.pow(10,d);	// transform the number into unsigned integers
+		let pres = 244 + d*s;
+		// console.log("Value:",value,"S:",s,"D:",d,"Uint:",uint,"Press:",pres);
+	    return [
+	        Math.floor(uint%255),
+	        Math.floor(uint/255)%255,
+	        Math.floor(uint/(255*255)),
+	        pres
+	    ];
+	}
+	else if (type === 'position' || type === 'vec2') {				
+		let x = value[0]*65025;
+		let y = value[1]*65025;
+		return [
+			Math.floor(x%255),
+	        Math.floor(x/255)%255,
+	        Math.floor(y%255),
+	        Math.floor(y/255)%255
+	    ];
+	}
+	else if (type === 'color' || type === 'rgb' || type === 'vec3' || type === 'vec4') {
+		return [
+	        Math.floor(value[0]*255),
+	        Math.floor(value[1]*255),
+	        Math.floor(value[2]*255),
+	        value[3] ? Math.floor(value[3]*255): 255
+	    ];
+	}
+}
+
+window.getRangeFor = getRangeFor;
