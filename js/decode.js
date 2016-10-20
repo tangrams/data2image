3c1f432810a195f8354b7142e7e342fcc6f8e5b5
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2016-10-20 16:30:14 -0400

    fixing errors

--- js/decode.js
+++ js/decode.js
@@ -7,7 +7,7 @@ vec2 getCoord(vec2 res, float col, float row) {
 
 var uint = common + `
 float getUInt(sampler2D tex, vec2 res, float col, float row) {
-    highp vec4 value = texture2D(tex, coord);
+    highp vec4 value = texture2D(tex, getCoord(res, col, row));
     return (value.x*255.)+(value.y*65025.)+(value.z*16581375.);
 }`;
 
@@ -19,9 +19,8 @@ float getUFloat(sampler2D tex, vec2 res, float col, float row) {
 
 var position = common + `
 vec2 getPosition(sampler2D tex, vec2 res, float col, float row) {
-    vec2 coord = getCoord(res, col, row);
-    highp vec4 value = texture2D(tex, coord);
-    float x = (value.r*255+value.g*65025.)/65025.;
+    highp vec4 value = texture2D(tex, getCoord(res, col, row));
+    float x = (value.r*255.+value.g*65025.)/65025.;
     float y = (value.b*255.+value.a*65025.)/65025.;
     return vec2(x,y);
 }`+'\n';
@@ -51,8 +50,7 @@ vec3 getElements(sampler2D tex, vec2 coord) {
     return vec3(uint, abs(press), sign(press));
 }
 float getNumber(sampler2D tex, vec2 res, float col, float row) {
-    vec2 coord = getCoord(res, col, row);
-    highp vec3 elements = getElements(tex, coord);
+    highp vec3 elements = getElements(tex, getCoord(res, col, row));
     return elements.x * pow(10.,-floor(elements.y)) * elements.z;
 }`+'\n',
     position: position,
